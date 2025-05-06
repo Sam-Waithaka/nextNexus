d5c91bb7eaed61b46c9e17d117bcf502d5b5383e
Sam-Waithaka <sw.waithakasamuel@gmail.com>
2025-05-06 14:04:11 +0300

    Add a form

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,44 @@
 export default function Contact(){
     return (
-        <main>
+        <main className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md">
             <div>
-                <h1>Contact Me!!</h1>
+                <h1 className="text-2xl font-bold text-center mb-6">Contact Me!!</h1>
+                <form className="space-y-4 ">
+                    <div>
+                        <label 
+                        htmlFor="email"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            Email
+                        </label>
+                        <input 
+                            id="email" 
+                            type="email" 
+                            name="email" 
+                            required 
+                            className="border border-gra300` focus:ring-2 focus:ring-blue-500 focus:border-blue-500"/>
+                    </div>
+                    <div>
+                        <label 
+                            htmlFor="message" 
+                            className="block text-sm font-medium text-gray-700"
+                        > 
+                            Message
+                        </label>
+                        <textarea 
+                            id="message" 
+                            required
+                            name="message"
+                            rows="4"
+                            className="border border-gra300` focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+                        ></textarea>
+                    </div>
+                    <button
+                        className="text-white bg-blue-500 rounded-md p-3"
+                    >
+                        Send Message
+                    </button>
+                </form>
             </div>
         </main>
     );
